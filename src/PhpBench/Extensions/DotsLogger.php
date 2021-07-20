fcf0964ea5b329527cc7d5c934f3c1435fc05e69
butschster <butschster@gmail.com>
2021-07-20 23:06:03 +0300

    Refactor
    - Added error logger

--- src/PhpBench/Extensions/DotsLogger.php
+++ src/PhpBench/Extensions/DotsLogger.php
@@ -3,12 +3,15 @@ declare(strict_types=1);
 
 namespace Cycle\Benchmarks\Base\PhpBench\Extensions;
 
+use Monolog\Formatter\LineFormatter;
 use PhpBench\Benchmark\RunnerConfig;
 use PhpBench\Model\Benchmark;
 use PhpBench\Model\Suite;
 use PhpBench\Progress\VariantFormatter;
 use PhpBench\Util\TimeUnit;
 use Symfony\Component\Console\Output\OutputInterface;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 class DotsLogger extends \PhpBench\Progress\Logger\DotsLogger
 {
@@ -47,12 +50,33 @@ class DotsLogger extends \PhpBench\Progress\Logger\DotsLogger
 
     public function startSuite(RunnerConfig $config, Suite $suite): void
     {
-
     }
 
     public function endSuite(Suite $suite): void
     {
+        $log = $this->createLogger();
 
+        $errorStacks = $suite->getErrorStacks();
+
+        foreach ($errorStacks as $errorStack) {
+            $stack = [sprintf(
+                "%s::%s",
+                $errorStack->getVariant()->getSubject()->getBenchmark()->getClass(),
+                $errorStack->getVariant()->getSubject()->getName()
+            )];
+
+            $error = $errorStack->getErrors()[0] ?? null;
+
+            if ($error) {
+                $stack[] = sprintf(
+                    "%s %s",
+                    $error->getMessage(),
+                    $error->getTrace()
+                );
+            }
+
+            $log->error(implode("\n", $stack)."\n\n==================== End of error ============================\n");
+        }
     }
 
 
@@ -78,8 +102,20 @@ class DotsLogger extends \PhpBench\Progress\Logger\DotsLogger
 
     public function studly(string $value)
     {
-        $value = ucwords(str_replace(['-', '_'], ' ', $value));
+        return ucwords(str_replace(['-', '_'], ' ', $value));
+    }
 
-        return $value;
+    protected function createLogger(): Logger
+    {
+        $log = new Logger('name');
+        $stream = new StreamHandler(ROOT . DIRECTORY_SEPARATOR . 'runtime' . DIRECTORY_SEPARATOR . 'logs' . DIRECTORY_SEPARATOR . 'phpbench.log');
+        $formatter = new LineFormatter(null, "Y-m-d H:i:s", true, true);
+        $formatter->includeStacktraces();
+
+        $stream->setFormatter($formatter);
+
+        $log->pushHandler($stream);
+
+        return $log;
     }
 }
